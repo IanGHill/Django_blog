0d32c00c7d0b8a1cf02f1a06428f1c3ee92419a2
Ian Hill <ian.g.hill@outlook.com>
2020-12-29 15:20:14 +0000

    testing git connection

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
